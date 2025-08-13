5137e43053e9a9c742940aaa6fa042c876004b03
Amala Jiagbogu <shanteldboss@gmail.com>
2025-08-13 17:53:49 -0500

    Ensured that users on the frontend can only view their chats after logging in. Chat text and memory persist even after refreshing and re-login by the users. Tomorrow, I need to set up a reset password flow and make sure they can only select famous people and not input random words (this may need to be a drop-down if it stresses me out too much)

---
+++ frontend/src/api.js
@@ -0,0 +1,35 @@
+// frontend/src/api.js
+const API_BASE = "http://localhost:5000/api";
+
+async function request(path, options = {}) {
+  console.log("[api.js] Making request to:", `${API_BASE}${path}`, options);
+  const token = localStorage.getItem("token");
+
+  const headers = {
+    "Content-Type": "application/json",
+    ...options.headers,
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  const res = await fetch(`${API_BASE}${path}`, {
+    ...options,
+    headers,
+  });
+
+  if (!res.ok) {
+    const errData = await res.json().catch(() => ({}));
+    throw new Error(errData.message || "Request failed");
+  }
+
+  return res.json();
+}
+
+export const api = {
+  get: (path) => request(path),
+  post: (path, body) => request(path, { method: "POST", body: JSON.stringify(body) }),
+  put: (path, body) => request(path, { method: "PUT", body: JSON.stringify(body) }),
+  delete: (path) => request(path, { method: "DELETE" }),
+};
