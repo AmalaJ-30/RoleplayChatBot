26073e46b77c9d39a772c36d0ad9e80eadb712c8
Amala Jiagbogu <shanteldboss@gmail.com>
2025-08-22 11:21:03 -0500

    Added a private route file to make sure only logged in users can access the /users and /profile pages, if you are not logged in or not a user, it directs you to the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,30 @@ import UserChat from "./pages/userChat/UserChat.jsx";
 import SignupEmailVerification from "./pages/signup/SignupEmailVerification.jsx";
 import ResetPassword from "./pages/signup/resetpasswordpage.jsx";
 import Profile from "./pages/userChat/Profile.jsx";
+import PrivateRoute from "./pages/userChat/PrivateRoute";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signup/verify" element={<SignupEmailVerification />} />
-      <Route path="/userChat" element={<UserChat />} />
+      <Route
+  path="/userchat"
+  element={
+    <PrivateRoute>
+      <UserChat />
+    </PrivateRoute>
+  }
+/>
       <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
 
 
     </Routes>
