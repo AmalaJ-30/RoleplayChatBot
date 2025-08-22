1d18b410ddf094db99592d37d408a1f24722248e
Amala Jiagbogu <shanteldboss@gmail.com>
2025-08-22 17:39:34 -0500

    replaced cors with temp link

--- auth-server/server.js
+++ auth-server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://stellar-pavlova-647c52.netlify.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
